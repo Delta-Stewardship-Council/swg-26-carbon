c70c1ff60bff96a314d0d9ab71836b0d9bb9423d
MeganPagliaro <megan.pagliaro@water.ca.gov>
2026-05-01 11:06:25 -0700

    more updates

--- megan.r
+++ megan.r
@@ -1,6 +1,10 @@
 #Megan test code
 
-
-
 #get setup
 source("-setup.r")
+
+#setting up link to boxdrive
+#for more info got to link below:
+#https://nceas-learning-hub.github.io/2026_delta_week1/s06_lecture_data_mgmt_discovery.html#bonus-load-data-from-box-into-r
+library(boxr)
+box_auth()
