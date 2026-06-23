9026980fffad156207e6851709744743ec2e0141
dianaverse <dzapataro@gmail.com>
2026-06-23 13:06:50 -0700

    deverel_maps

--- scripts/01_delta_hydrofocus_maps.r
+++ scripts/01_delta_hydrofocus_maps.r
@@ -1,13 +1,8 @@
----
-title: "Read Hydrofocus Maps"
-format: html
-author: Diana Zapata
----
+### Script for reading Hydrofocus maps from Box
 
-## Setup
+### Setup
 source("tools/-setup.r") # This sources functions within the setup script to make sure we all have the same folder structure
 source("tools/box_authentication.R") # This will authenticate your Box connection to be able to access data. info on specific Box folder IDs is also in this script for your reference
-
 
 ### Download packages
 install.packages(c("boxr", "sf", "terra"))
@@ -21,7 +16,7 @@ library(terra)
 # box_ls("392886560802") # Hydrofocus
 
 ### Download data
- # 1. Define your Box folder ID
+# 1. Define your Box folder ID
 deverel_box_folder <- "392886560802"
 
 # 2. Create a local temporary directory to store the spatial files
@@ -34,13 +29,21 @@ box_fetch(
     overwrite = TRUE
 )
 
-###
+### Read the raster files
 # 1. Find the raster file name in your temporary directory
 # This looks for any file ending in .tif or .tiff
 raster_file <- list.files(local_dir, pattern = "\\.tiff?$", full.names = TRUE)
 
 # Print it to make sure R found it
 print(raster_file)
+
+# Get to know each raster structure
+raster_list[1] # Delta_Wide_flux_Year1_tCO2e_ac_v04152025
+raster_list[2] # DTW_delta_wide_meters_05302025
+raster_list[3] # org_soil_thickness_2017_meters_org_ext_05302025
+
+my_stack <- rast(raster_file)
+
 
 # Load them as a collection list instead of a single stack
 raster_list <- lapply(raster_file, rast)
