deb9efad096782c66edca9aede5142aa7eb3c4b1
tarapozzi <tpozzi@ucsd.edu>
2026-06-26 10:25:47 -0700

    added code to send rasters to clean data file

--- scripts/01_example_LSPT_script.R
+++ scripts/01_example_LSPT_script.R
@@ -1,9 +1,14 @@
+library(boxr)
+library(terra)
+library(sf)
+
 source("tools/-setup.r") #Setup Script
 source("tools/box_authentication.R")
+
 box_ls('392885596000') #Checking out the contents of the Landscape Scenario Planning Tool
 box_fetch(
   dir_id = '392885596000', #putting in tiff file id
-  local_dir = file.path('data/LSPT'), #setting in the data folder
+  local_dir = file.path('data'), #setting in the data folder
   recursive = TRUE,
   overwrite = FALSE,
   delete = FALSE
@@ -13,11 +18,13 @@ box_fetch(
 # This looks for any file ending in .tif or .tiff
 raster_file <- list.files(path= 'data', pattern = "\\.tiff?$", full.names = TRUE)
 print(raster_file)
+
 raster_list <- lapply(raster_file, rast)
 raster_list[1] #carbon_storage_hist
 raster_list[2] #carbon_storage_mod
 raster_list[3] #peat_thickness_hist
 raster_list[4] #peat_thickness_mod
+
 carbon_storage_hist<-rast(raster_list[1])
 plot(carbon_storage_hist)
 carbon_storage_mod<-rast(raster_list[2])
@@ -26,3 +33,36 @@ peat_thickness_hist<-rast(raster_list[3])
 plot(peat_thickness_hist)
 peat_thickness_mod<-rast(raster_list[4])
 plot(peat_thickness_mod)
+
+# Export rasters to Box
+# Temporarily store raster
+temp_raster_path <- file.path(tempdir(), "carbon_storage_hist.tif")
+writeRaster(carbon_storage_hist, temp_raster_path, overwrite = TRUE)
+
+# Define your Box folder number (ID) and upload the file
+folder_id_number <- "379716878316" # Replace with your actual Box folder ID
+box_ul(file = temp_raster_path, dir_id = 379716878316)
+
+# Temporarily store raster
+temp_raster_path <- file.path(tempdir(), "carbon_storage_mod.tif")
+writeRaster(carbon_storage_mod, temp_raster_path, overwrite = TRUE)
+
+# Define your Box folder number (ID) and upload the file
+folder_id_number <- "379716878316" # Replace with your actual Box folder ID
+box_ul(file = temp_raster_path, dir_id = 379716878316)
+
+# Temporarily store raster
+temp_raster_path <- file.path(tempdir(), "peat_thickness_hist.tif")
+writeRaster(peat_thickness_hist, temp_raster_path, overwrite = TRUE)
+
+# Define your Box folder number (ID) and upload the file
+folder_id_number <- "379716878316" # Replace with your actual Box folder ID
+box_ul(file = temp_raster_path, dir_id = 379716878316)
+
+# Temporarily store raster
+temp_raster_path <- file.path(tempdir(), "peat_thickness_mod.tif")
+writeRaster(peat_thickness_mod, temp_raster_path, overwrite = TRUE)
+
+# Define your Box folder number (ID) and upload the file
+folder_id_number <- "379716878316" # Replace with your actual Box folder ID
+box_ul(file = temp_raster_path, dir_id = 379716878316)
